c69c9fcf887cc31f598a1171334f8deb360f30a8
Hashim <hsqworld@hotmail.com>
2022-11-04 02:57:02 +0500

    Created a readme file.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import pic from '../src/images.png'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const page = (
+    <div>
+        <img src={pic}alt='BigCo Inc logo'/>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>It's composbile</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintaind by skilled people</li>
+        </ul>
+    </div>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(
+    page,
+    document.getElementById('root')
+)
